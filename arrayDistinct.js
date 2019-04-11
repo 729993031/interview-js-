f24121db053f23c4cb2d78830562eda917f89837
Administrator <729993031@qq.com>
2019-04-11 21:01:27 +0800

    数组去重

--- arrayDistinct.js
+++ arrayDistinct.js
@@ -27,11 +27,22 @@ function distance_map(array) {
     array.filter((element)=>!array_filtration.has(element)&&array_filtration.set(element,1));
     return console.timeEnd('Map');
 }
+function distance_object(array) {
+    console.time('object');
+    let resultObject={};
+    array.map((element)=>{
+        if (element in resultObject !==true){
+            resultObject[element]=true;
+        }
+    })
+    return console.timeEnd('object');
+}
 const target_array=[];
 for (let i = 0; i < 100000; i++) {
-    target_array.push(0 + Math.floor((100000 - 0 + 1) * Math.random()))
+    target_array.push( Math.floor(100001 * Math.random()))
 }
-distance_forEach(target_array);
-distance_filter(target_array);
+// distance_forEach(target_array);
+// distance_filter(target_array);
+distance_object(target_array);
 distance_map(target_array);
 
