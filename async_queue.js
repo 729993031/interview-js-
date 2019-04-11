f4d2f37fd00fab7b5a9d36cda0145de70cbb7ef8
Administrator <729993031@qq.com>
2019-04-11 21:07:11 +0800

    async、promise、setTimeout多种函数运行

--- async_queue.js
+++ async_queue.js
@@ -1,4 +1,8 @@
 async function async1() {
+    /*
+    async如果没有await异步的时候就是同步事件
+
+     */
     console.log('async1 start');
     await async2();
     console.log('async1 end');
@@ -11,7 +15,7 @@ console.log('script start');
 
 setTimeout(function() {
     console.log('setTimeout');
-}, 0)
+}, 0);
 
 async1();
 
@@ -22,3 +26,13 @@ new Promise(function(resolve) {
     console.log('promise2');
 });
 console.log('script end');
+/*
+    script start
+    async 1 start
+    async 2 start
+    promise1
+    script end
+    promise2
+    async1 end
+    setTimeout
+ */
---
+++ repetition_value.js
@@ -0,0 +1,38 @@
+function filter_repetition(array) {
+    const array_filtration=new Map();
+    let result;
+    if (array.length===0){
+         result=false;
+    } else {
+        for ( var i=0;i<array.length;i++){
+            if (array_filtration.has(array[i])){
+                return console.log(result='true');
+            }else {
+                array_filtration.set(array[i],'1');
+                result='false';
+            }
+        }
+    }
+    console.log (result);
+}
+let array_test=[1,2,3,1];
+console.time('map');
+filter_repetition(array_test);
+console.timeEnd('map');
+function filter_repetition (nums) {
+    const map = {};
+    for (const v of nums) {
+        if (v in map) {
+            return true
+        }
+        map[v] = true
+    }
+    return false
+};
+console.time('map');
+filter_repetition(array_test);
+console.timeEnd('map');
+/*
+map: 0.541ms
+map: 0.017ms
+ */
